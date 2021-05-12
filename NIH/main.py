ae6e40737b099ce9d0d7e93b871e937d09843a1b
Roman Lutz <rolutz@microsoft.com>
2021-05-12 14:55:57 -0700

    print options
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- NIH/main.py
+++ NIH/main.py
@@ -141,6 +141,11 @@ def preprocess_NIH(df):
         df[disease] = df['Finding Labels'].apply(lambda labels: 1 if disease in labels else 0)
         total_label_count += df[disease]
 
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', -1)
+
     print(df.head(n=100))
 
     print(df.where(total_label_count == df['Finding Labels'].apply(lambda labels: len(labels.split("|")))))
